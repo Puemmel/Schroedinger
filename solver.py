b0322f651f4557d217aa5812fc562549f397762c
R1ngularity <104900862+R1ngularity@users.noreply.github.com>
2022-10-07 13:20:40 +0200

    added expvalues and removed plot

--- solver.py
+++ solver.py
@@ -3,21 +3,21 @@ import matplotlib.pyplot as plt
 import os
 from scipy import linalg
 import scipy.interpolate as interpolate
-
+import math
 
  
 
 def schroedinger_solver():
     """
+    solves the schroedinger equation with the interpolated potential via eigenvalue problem.
     
-
-    Returns
     -------
-    None.
-
+    returns wavefuncs.dat
+            energies.dat
+            expvalues.dat
+    
+    
     """
-
-     
 
     f = open("schroedinger.inp", "r")
     inp = []
@@ -34,7 +34,6 @@ def schroedinger_solver():
     diag = np.empty(n, dtype = float)
     offdiag = np.full(n-1, -0.5*a)
     
-    
     f2 = open("potential.dat", "r")
     inppot = []
     for y in f2:
@@ -44,27 +43,30 @@ def schroedinger_solver():
     for i in range(n):
         diag[i] = inppot[i][1]
     
-    
-    
     eigenvalues,eigenvectormatrix = linalg.eigh_tridiagonal(diag, offdiag)
-    
-    
     
     x = np.linspace(float(inp[1][0]), float(inp[1][1]), int(inp[1][2]))
     d = [[] for x in range(int(inp[2][0]),int(inp[2][1]) + 1)]
     
     
-    """
-    norm = []
+    
+
+    quamatrix = np.empty((n, n))
     for j in range(n):
-        norm.append(delta*sum((abs(eigenvectormatrix[j]))**2))
-    print(norm)
-    """
-    print(eigenvectormatrix)
-        
+        for i in range(n):
+            quamatrix[j][i] = (abs(eigenvectormatrix[j][i])**2)
+
+    normmat = np.sum(quamatrix, axis=1)
+    
+
+
     for j in range(len(d)):
         for i in range(n):
-            d[j].append(1/(delta*sum((abs(eigenvectormatrix[j]))**2)) * eigenvectormatrix[i][j])
+            d[j].append(delta/(normmat[j]) * eigenvectormatrix[i][j])
+   
+    
+    
+    
     
     
     
@@ -83,6 +85,10 @@ def schroedinger_solver():
     f2.close()
     
     
+    
+    
+    
+    
     f3 = open("energies.dat", "w")
     for i in range(int(inp[2][0]),int(inp[2][1]) + 1):
         f3.write(str(eigenvalues[i]))
@@ -91,7 +97,45 @@ def schroedinger_solver():
     
     
     
-    for i in range(len(d)):    
-        plt.plot(x, d[i])
-    plt.show()
+    
+    
+    
+    
+    # calculate ortsop and sigma
+    
+    expx = [[] for i in range(len(d))]
+    expxx = [[] for i in range(len(d))]
+    
+    for j in range(len(d)):
+        for i in range(n):
+            k = d[j][i] * wavexpoints[i] * d[j][i] 
+            expx[j].append(k)
+            
+    
+    for j in range(len(d)):
+        for i in range(n):
+            k = d[j][i] * (wavexpoints[i])**2 * d[j][i] 
+            expxx[j].append(k)
+
+    for j in range(len(d)):
+        expx[j] = delta * sum(expx[j])
+        expxx[j] = delta * sum(expxx[j])
+   
+    
+   
+    sigma = []
+    for i in range(len(expx)):
+        sigma.append(math.sqrt(expxx[i] - (expx[i])**2))
+    
+    
+    
+    
+    
+    f4 = open("expvalues.dat", "w")
+    for i in range(len(sigma)):
+        f4.write(str(sigma[i]))
+        f4.write("\n")
+    f4.close()
+    
+    
     
