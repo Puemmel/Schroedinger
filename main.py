6d4f0808b9c942aea1a05ec478c9df03dae6d4d3
Eike Krull <eike.krull@gmx.de>
2022-10-12 15:21:46 +0200

    Adding the directory choice by user

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 """
 main-script to open functions and create the wanted output
 """
-import os
+import os.path
 import plotter as pt
 import interinterpolator as pol
 import solver as sol
 
+print(os.getcwd())
+
 answer = input("Is the file in the current path [y/n]?")
 
-if answer != "n":
-    print('getcwd:      ', os.getcwd())
+if answer == "y":
+    print('cwd:      ', os.getcwd())
 
-else:
-    file_path = input('Enter a file path: ')
-
-    # e.g. C:\Users\Bob\Desktop\example.txt
-    # or /home/Bob/Desktop/example.txt
-    print(file_path)
-
-    if os.path.exists(file_path):
-        print('The file exists')
-
-        with open(file_path, 'r', encoding='utf-8-sig') as f:
-            lines = f.readlines()
-
-            print(lines)
+elif answer =='n':
+    path = input("Enter a file path:" )
+    try:
+        os.chdir(path)
+        print(path)
+    except FileNotFoundError:
+        print("Directory: {0} does not exist".format(path))
+    except NotADirectoryError:
+        print("{0} is not a directory".format(path))
+    except PermissionError:
+        print("You do not have permissions to change to {0}".format(path))
     else:
-        print('The specified file does NOT exist')
+        print('Seomething didnt work out')
+    # e.g. C:\Users\Bob\Desktop
+    # or /home/Bob/Desktop
+else:
+    print('You have to enter y or n')
 
 pt.interpol()
 sol.schroedinger_solver()
